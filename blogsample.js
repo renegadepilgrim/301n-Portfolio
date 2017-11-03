f8979afa6b7a1734d0515ba87f8993d833e1ee12
Heather Knight <heather.knight.pdx@gmail.com>
2017-11-02 20:00:21 -0700

    added photo to about.html page.  Still can't get the dropdown nav bar to work.

---
+++ blogsample.js
@@ -0,0 +1,14 @@
+var rawData = [
+  {
+    title:       'Mmmmm, Bacon',
+    author:      'Porky Pig',
+    publishedOn: '2017-10-05',
+    body:        '<p>Sample Text Here.</p>'
+  },
+  {
+    title:       'I Love Cheese',
+    author:      'Mickey Mouse',
+    publishedOn: '2017-11-01',
+    body:        '<p>More Sample Text.</p>'
+  },
+];
